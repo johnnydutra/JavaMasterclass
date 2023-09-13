4c5aeb5db7901de82598c70bfc611caf3620e93b
Johnny Dutra <johnny.dutra@gmail.com>
2023-09-13 16:42:39 -0300

    adds Encapsulation content and challenge

---
+++ ObjectOrientationPart2/src/encapsulation/challenge/Main.java
@@ -0,0 +1,17 @@
+package encapsulation.challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, false);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
+
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current job pages: %d, Printer total: %d %n", pagesPrinted, printer.getPagesPrinted());
+
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current job pages: %d, Printer total: %d %n", pagesPrinted, printer.getPagesPrinted());
+
+    }
+}
---
+++ ObjectOrientationPart2/src/encapsulation/theory/EnhancedPlayer.java
@@ -0,0 +1,28 @@
+package encapsulation.theory;
+
+public class EnhancedPlayer {
+
+    private String fullName;
+    private int healthPercentage;
+    private String weapon;
+
+    public EnhancedPlayer(String fullName) {
+        this(fullName, 100, "Sword");
+    }
+
+    public EnhancedPlayer(String fullName, int healthPercentage, String weapon) {
+        this.fullName = fullName;
+        if (healthPercentage <= 0) {
+            this.healthPercentage = -1;
+        } else if (healthPercentage > 100) {
+            this.healthPercentage = 100;
+        } else {
+            this.healthPercentage = healthPercentage;
+        }
+        this.weapon = weapon;
+    }
+
+    public int healthRemaining() {
+        return healthPercentage;
+    }
+}
